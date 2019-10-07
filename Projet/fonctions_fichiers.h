6933d123f5ee785cb641794d2342e00fed3169e5
Pierre Marty <marty26u@fst-ats-309-l18.fst.site.univ-lorraine.fr>
2019-10-07 17:11:17 +0200

    Creation des fichiers du deuxieme TP

---
+++ Projet/fonctions_fichiers.h
@@ -0,0 +1,13 @@
+#ifndef FICH
+#define FICH
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char** allouer_tab_2D(int n,int m);
+void desallouer_tab_2D(char** tab, int n);
+void afficher_tab_2D(char** tab, int n, int m);
+void taille_fichier(const char* nomFichier, int* nbLig, int* nbCol);
+char** lire_fichier(const char* nomFichier);
+
+#endif
